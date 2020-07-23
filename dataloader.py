de9cc96ab3aacb6c3daeebfd9ee6480b686d5f09
nishidayoshikatsu <nsd24technology@gmail.com>
2020-07-23 14:16:07 +0900

    44, 45例目のnewsを更新

--- dataloader.py
+++ dataloader.py
@@ -422,9 +422,9 @@ class GraphData:
         prev_data["newsItems"].insert(
             insert_index,
 			{
-                "date": "2020/07/18",
-                "text": "山口県内で42例目となる新型コロナウイルス感染症の感染者を確認",
-                "url": "https://www.pref.yamaguchi.lg.jp/cms/a15200/kansensyou/202007170004.html"
+                "date": "2020/07/23",
+                "text": "山口県内で44, 45例目となる新型コロナウイルス感染症の感染者を確認",
+                "url": "https://www.pref.yamaguchi.lg.jp/cms/a15200/kansensyou/202007230001.html"
             }
 		)
 
