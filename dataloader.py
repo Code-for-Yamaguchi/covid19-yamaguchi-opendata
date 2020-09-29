69872479f923dbf0cdbb46fc1f7b322a86cd4604
NSD <nsd24technology@gmail.com>
2020-09-29 15:58:42 +0900

    197, 198例目のnewsを追加

--- dataloader.py
+++ dataloader.py
@@ -449,9 +449,9 @@ class GraphData:
         prev_data["newsItems"].insert(
             insert_index,
 			{
-                "date": "2020/09/26",
-                "text": "山口県内で196例目となる新型コロナウイルス感染症の感染者を確認",
-                "url": "https://www.pref.yamaguchi.lg.jp/cms/a15200/kansensyou/202009260001.html"
+                "date": "2020/09/27",
+                "text": "山口県内で197、198例目となる新型コロナウイルス感染症の感染者を確認",
+                "url": "https://www.pref.yamaguchi.lg.jp/cms/a15200/kansensyou/202009270002.html"
             }
 		)
 
