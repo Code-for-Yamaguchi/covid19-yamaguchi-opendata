fe3ef31fdb7a8c90cb28b2bd36ebc3207f896e48
nishidayoshikatsu <nsd24technology@gmail.com>
2020-12-21 17:55:38 +0900

    467~470例目のnewsを追加

--- dataloader.py
+++ dataloader.py
@@ -450,9 +450,9 @@ class GraphData:
         prev_data["newsItems"].insert(
             insert_index,
 			{
-                "date": "2020/12/18",
-                "text": "山口県内で448~455例目となる新型コロナウイルス感染症の感染者を確認",
-                "url": "https://www.pref.yamaguchi.lg.jp/cms/a15200/kansensyou/202012180001.html"
+                "date": "2020/12/21",
+                "text": "山口県内で467~470例目となる新型コロナウイルス感染症の感染者を確認",
+                "url": "https://www.pref.yamaguchi.lg.jp/cms/a15200/kansensyou/202012210001.html"
             }
 		)
 
