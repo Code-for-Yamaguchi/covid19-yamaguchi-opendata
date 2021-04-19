e90919bd5f472175079e4290357bab27e5b454e9
nishidayoshikatsu <nsd24technology@gmail.com>
2021-04-20 06:31:21 +0900

    検査実施件数データの更新を中止

--- config.py
+++ config.py
@@ -8,10 +8,6 @@ REMOTE_SOURCES = {
         'url':'https://yamaguchi-opendata.jp/ckan/dataset/f6e5cff9-ae43-4cd9-a398-085187277edf/resource/2b48b14f-2221-4b42-9a7d-78ab89641d23/download/350001_yamaguchi_covid19_test_people.csv',
         'type':'csv'
     },
-    'inspections':{
-        'url':'https://yamaguchi-opendata.jp/ckan/dataset/f6e5cff9-ae43-4cd9-a398-085187277edf/resource/21b7caeb-05b2-401b-8245-28757de8f444/download/350001_yamaguchi_covid19_test_count.csv',
-        'type':'csv'
-	},
     'hospitalizations':{
         'url':'https://yamaguchi-opendata.jp/ckan/dataset/f6e5cff9-ae43-4cd9-a398-085187277edf/resource/1a5f9bca-3216-45df-8a99-5c591df8f628/download/350001_yamaguchi_covid19_hospitalization.csv',
         'type':'csv'
